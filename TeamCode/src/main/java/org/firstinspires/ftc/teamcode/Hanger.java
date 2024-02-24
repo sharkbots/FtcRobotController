19ad6b348c34769e531460f88110062a8151b63b
ACH00M <justgingrich@gmail.com>
2024-02-24 12:15:51 -0500

    hanger changed to only use motor encoder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hanger.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hanger.java
@@ -8,17 +8,22 @@ import com.qualcomm.robotcore.hardware.TouchSensor;
 import org.firstinspires.ftc.teamcode.tools.Button;
 
 public class Hanger {
-    public final TouchSensor hangerTouchUp;
     private final DcMotor hangerMotor;
+    private final DcMotor hangerEncoder;
     private final int countsPerRev = 1531; //not sure of this value
     private final double HANGER_REVOLUTIONS = 0.4; //need to test to see how many revolutions
     private final Button handlerDPadDown, handlerDPadUp;
 
+    private final int maxSkyHookPosition = 10800;
+    private final int minSkyHookPosition = 100;
+
 
     public Hanger(HardwareMap hardwareMap, Button handlerDPadDown, Button handlerDPadUp) {
         hangerMotor = hardwareMap.dcMotor.get("skyHookMotor");
-        hangerTouchUp = hardwareMap.touchSensor.get("skyHookTouchUp");
         hangerMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+
+        // Hanger encoder is plugged into a different port than the hanger motor
+        hangerEncoder = hardwareMap.dcMotor.get("backRightMotor");
         //hangerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
 
         this.handlerDPadDown = handlerDPadDown;
@@ -28,7 +33,7 @@ public class Hanger {
     public void update(Button button) {
         //TelemetryManager.getTelemetry().addData("Hanger Pos: ", hangerMotor.getCurrentPosition());
         if(handlerDPadUp.On()){
-            if(hangerTouchUp.isPressed()) {
+            if(hangerEncoder.getCurrentPosition() >= maxSkyHookPosition) {
                 hangerMotor.setPower(0);
             }
             else {
@@ -42,12 +47,14 @@ public class Hanger {
             //int targetPosition = (int)(countsPerRev * HANGER_REVOLUTIONS); // cast to int
             //hangerMotor.setTargetPosition(0);
             //hangerMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-            hangerMotor.setPower(-1);
-        }
-        else {
-            hangerMotor.setPower(0);
-            //hangerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
-            //hangerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            if(hangerEncoder.getCurrentPosition() <= minSkyHookPosition) {
+                hangerMotor.setPower(0);
+            }
+            else {
+                hangerMotor.setPower(-1);
+                //hangerMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+                //hangerMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            }
         }
     }
 
