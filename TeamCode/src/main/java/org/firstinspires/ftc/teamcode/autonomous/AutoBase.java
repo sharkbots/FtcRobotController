e4f5aa78220f020debdd4b2714fd64538018f3b9
VincentDiv <vincent.revise11@outlook.com>
2024-04-16 10:59:24 -0400

    all 3 trajectories for purple

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBase.java
@@ -410,8 +410,26 @@ public abstract class AutoBase extends LinearOpMode {
                .build();
 
 
-        TrajectorySequence purpleAndStack = drive.trajectorySequenceBuilder(new Pose2d(12.00, 62.00, Math.toRadians(90.00)))
+        TrajectorySequence purpleCenterAndStack = drive.trajectorySequenceBuilder(new Pose2d(12.00, 62.00, Math.toRadians(90.00)))
                 .lineTo(new Vector2d(12.00, 34.00))
+                .lineToLinearHeading(new Pose2d(12.00, 59.00, Math.toRadians(180.00)))
+                .lineToLinearHeading(new Pose2d(-32.00, 60.00, Math.toRadians(198.00)))
+                .lineToLinearHeading(c.stackRightSetup)
+                .lineToLinearHeading(c.stackRight)
+                .build();
+
+        TrajectorySequence purpleLeftAndStack = drive.trajectorySequenceBuilder(new Pose2d(12.00, 62.00, Math.toRadians(90.00)))
+                .lineTo(new Vector2d(12.00, 34.00))
+                .lineTo(new Vector2d(10.00, 34.00))
+                .lineToLinearHeading(new Pose2d(12.00, 59.00, Math.toRadians(180.00)))
+                .lineToLinearHeading(new Pose2d(-32.00, 60.00, Math.toRadians(198.00)))
+                .lineToLinearHeading(c.stackRightSetup)
+                .lineToLinearHeading(c.stackRight)
+                .build();
+
+        TrajectorySequence purpleRightAndStack = drive.trajectorySequenceBuilder(new Pose2d(12.00, 62.00, Math.toRadians(90.00)))
+                .lineToLinearHeading(new Pose2d(12.00, 34.00, Math.toRadians(0.00)))
+                .lineTo(new Vector2d(14.00, 34.00))
                 .lineToLinearHeading(new Pose2d(12.00, 59.00, Math.toRadians(180.00)))
                 .lineToLinearHeading(new Pose2d(-32.00, 60.00, Math.toRadians(198.00)))
                 .lineToLinearHeading(c.stackRightSetup)
@@ -420,7 +438,8 @@ public abstract class AutoBase extends LinearOpMode {
 
 
 
-        drive.followTrajectorySequence(purpleAndStack);
+
+        drive.followTrajectorySequence(purpleCenterAndStack);
 
 
 
