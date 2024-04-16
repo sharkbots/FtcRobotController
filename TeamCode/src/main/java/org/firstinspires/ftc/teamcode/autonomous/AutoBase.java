159fad27aeaa9456f8a68f464352df679c89dbf5
VincentDiv <vincent.revise11@outlook.com>
2024-04-16 18:17:41 -0400

    Left purple pixel trajectories

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonomous/AutoBase.java
@@ -422,14 +422,20 @@ public class AutoBase extends LinearOpMode {
 
         waitForStart();
 
-        TrajectorySequence purpleLeft = drive.trajectorySequenceBuilder(new Pose2d(12.00, 60.00, Math.toRadians(90.00)))
+        TrajectorySequence purpleLeftStraight = drive.trajectorySequenceBuilder(new Pose2d(12.00, 60.00, Math.toRadians(90.00)))
                 .lineTo(new Vector2d(19.30, 34.00))
                 .lineTo(new Vector2d(19.30, 40.00))
                 .lineToLinearHeading(c.backdropLeft)
                 .build();
 
+        TrajectorySequence purpleLeftTurn = drive.trajectorySequenceBuilder(new Pose2d(12.00, 60.00, Math.toRadians(90.00)))
+                .lineToLinearHeading(new Pose2d(14.00, 30.00, Math.toRadians(180.00)))
+                .lineTo(new Vector2d(10.00, 30.00))
+                .lineTo(new Vector2d(13.17, 9.20))
+                .build();
 
-        drive.followTrajectorySequence(purpleLeft);
+
+        drive.followTrajectorySequence(purpleLeftStraight);
 
 
 //        drive.followTrajectorySequence(backdropSideCenterPurple);
